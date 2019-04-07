948717f759df1db11ffd496167e7d3ea48dd0f81
taojian <taojian86@yahoo.com>
2019-04-07 12:51:26 +0800

    adding styling with stylesheet

--- src/Person.js
+++ src/Person.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import './person.css';
 
 // ES6 语法，不要使用以前的语法
 
 // return some jsx
 const person = (props) => {
     return (
-    <div>
+    <div className="person">
         <p onClick={props.click}>i am {props.name}, and i'm {props.age} years old</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
