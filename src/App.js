60034dbc3a6d5bd489cf938136151677c410930b
taojian <taojian86@yahoo.com>
2019-04-07 12:03:11 +0800

    删除ideaa

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ class App extends Component {
             {name: 'Tao', age: Math.floor(Math.random() * 30)},
         ]
     }
-    swithNameHandler = () => {
+    swithNameHandler = (newName) => {
         // alert('this is clicked');
         // DONT DO THIS
         // this.state.persons[0] = 'taojian'
         this.setState({
             persons: [
                 {name: 'Pole', age: Math.floor(Math.random() * 30)},
-                {name: 'Jia', age: Math.floor(Math.random() * 30)},
+                {name: newName, age: Math.floor(Math.random() * 30)},
                 {name: 'Lin', age: Math.floor(Math.random() * 30)},
             ]
         });
@@ -29,10 +29,15 @@ class App extends Component {
             <div className="App">
                 <h1>Cleaver Brooks</h1>
                 <p>this is the client</p>
-                <button onClick={this.swithNameHandler}>Swith Names</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.swithNameHandler}> i have a hobby </Person>
+                <button onClick={this.swithNameHandler.bind(this, 'Zhou Zhi')}>Swith Names</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.swithNameHandler.bind(this, 'Tao Jian')}> i have a hobby </Person>
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                    click={()=>this.swithNameHandler('666')}
+
+                />
             </div>
         );
     }
