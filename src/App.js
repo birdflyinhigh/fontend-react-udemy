c3792ab54f8bc32f744c6ed5ce1540bd8061f1ef
taojian <taojian86@yahoo.com>
2019-04-07 13:09:52 +0800

    adding two types of styling

--- src/App.js
+++ src/App.js
@@ -36,24 +36,38 @@ class App extends Component {
 
 
     render() {
+
+        const style={
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor:'pointer'
+        }
+
+
+
         return (
             <div className="App">
                 <h1>Cleaver Brooks</h1>
                 <p>this is the client</p>
-                <button onClick={this.swithNameHandler.bind(this, 'Zhou Zhi')}>Swith Names</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.swithNameHandler.bind(this, 'Tao Jian')}> i have a hobby </Person>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-                <Person
-                    name={this.state.persons[2].name}
-                    age={this.state.persons[2].age}
-                    click={()=>this.swithNameHandler('666')}
+                <button style={style}
+                    onClick={this.swithNameHandler.bind(this, 'Zhou Zhi')}>Swith Names</button>
+                <div>
+                    <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.swithNameHandler.bind(this, 'Tao Jian')}> i have a hobby </Person>
+                    <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age}
+                        click={()=>this.swithNameHandler('666')}
+                    />
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        changed={this.nameChangeHandler}
+                    />
+                </div>
 
-                />
-                <Person
-                    name={this.state.persons[1].name}
-                    age={this.state.persons[1].age}
-                    changed={this.nameChangeHandler}
-                />
             </div>
         );
     }
